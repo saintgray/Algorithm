320e66248dd035d64584ee763ccf6f0f65f7ffa4
OJH <saintgray0@gmail.com>
2022-08-15 14:19:17 +0900

    BaekJoon - Mathmatics 소수찾기
    // 참고 : 아리스토테네스의 체

---
+++ src/mathmatics/소수찾기2.java
@@ -0,0 +1,35 @@
+package mathmatics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+//    문제
+//    M 과 N 사이의 수 중 소수를 출력하는 프로그램을 작성하시오
+
+
+public class 소수찾기2 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        int[] numbers = Arrays.stream(in.readLine().split(" ")).mapToInt(e -> Integer.parseInt(e)).toArray();
+
+        for(int number : numbers){
+
+            boolean isPrimeNumber = true;
+
+            for(int i=1; i<=number; i++){
+                if(i!=1 && i!=number && number%i ==0){
+                    isPrimeNumber = false;
+                    break;
+                }
+            }
+
+            if(number ==1 || (number !=1 && !isPrimeNumber))
+                N--;
+        }
+        System.out.println(N);
+    }
+}
