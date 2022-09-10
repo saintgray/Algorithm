cf18bb84f7ffa9f0b472ca15bee99c1a68209366
OJH <saintgray0@gmail.com>
2022-09-10 10:57:31 +0900

    BaekJoon : sorting 10989 : 수 정렬하기 3

--- src/sorting/수정렬하기.java
+++ src/sorting/수정렬하기.java
@@ -1,58 +1,45 @@
 package sorting;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-
+//  10989 : 수 정렬하기 3
+//
 //	문제
 //	N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
-//	
+//
 //	입력
-//	첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수 주어진다. 
-//	이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
-//	
+//	첫째 줄에 수의 개수 N(1 ≤ N ≤ 10,000,000)이 주어진다.
+//	둘째 줄부터 N개의 줄에는 수가 주어진다.
+//	이 수는 10,000보다 작거나 같은 자연수이다.
+//
 //	출력
 //	첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
 public class 수정렬하기 {
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		List<Integer> array = new LinkedList<Integer>();
-		int size = Integer.parseInt(in.readLine());
-		for(int i=0; i<size; i++) {
-			array.add(Integer.parseInt(in.readLine()));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		Map<Integer, Integer> cntMap = new HashMap<>();
+		int N = Integer.parseInt(in.readLine());
+		for (int i = 0; i < N; i++) {
+			int n = Integer.parseInt(in.readLine());
+			Integer cnt = cntMap.get(n);
+			cntMap.put(n, cnt == null ? 1 : cnt + 1);
 		}
-		
-		Collections.sort(array, (o1, o2) -> o1>o2?1:o1==o2?0:-1);
 
-		// test case
-		// N = 5; numbers : 1, 1, 5, 4, 3
-
-
-		// filter 사용법
-		// collect 사용법
-//		 array = array.stream().filter(num->num==1).collect(Collectors.toList());
-		// expected result = 1,1
-
-
-		// addAll(Collections ..) 과 collect 의 혼합사용
-//		 array.addAll(array.stream().collect(Collectors.toList()));
-		// expected result = 1, 1, 3, 4, 5, 1, 1, 3, 4, 5
-
-
-		// map 객체 사용법
-//		 array = array.stream().map(obj->obj*3).collect(Collectors.toList());
-		// expected result = 3, 3, 9, 12, 15
-
-		
-		for(Integer num : array) {
-			System.out.printf("%d\n",num);
-		}
+		cntMap.keySet().stream().sorted().forEach(n -> {
+			for (int j = 0; j < cntMap.get(n); j++) {
+				try {
+					out.write(n + "\n");
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
+		out.flush();
 		in.close();
+		out.close();
 	}
-
 }
