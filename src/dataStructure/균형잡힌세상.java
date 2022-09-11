b5c5efd2ad17f5d187ff4e277515013796edb7b8
OJH <saintgray0@gmail.com>
2022-09-12 05:02:42 +0900

    BaekJoon : dataStructure 4949 : 균형잡힌 세상

--- src/dataStructure/균형잡힌세상.java
+++ src/dataStructure/균형잡힌세상.java
@@ -8,7 +8,7 @@ import java.util.Stack;
 //	4949 : 균형잡힌 세상
 //	ref url : https://www.acmicpc.net/problem/4949
 
-public class Main {
+public class 균형잡힌세상 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -19,24 +19,20 @@ public class Main {
             boolean balanced = true;
             for (int i = 0; i < input.length(); i++) {
                 char c = input.charAt(i);
-                if (stack.empty()){
-                    if(c == '[' || c == ']' || c == '(' || c == ')')
-                        stack.push(c);
-                }else{
+                if (!(c == '[' || c == ']' || c == '(' || c == ')'))
+                    continue;
+                if (stack.empty()) {
+                    stack.push(c);
+                } else {
                     char peek = stack.peek();
-                    if((c == ']' && peek =='[') || (c == ')' && peek =='(')){
+                    if ((c == ']' && peek == '[') || (c == ')' && peek == '(')) {
                         stack.pop();
-                    }else if ((c == '[' && peek =='(') || (c == '(' && peek == '[') || (c == ')' && peek ==']') || (c == ']' && peek == ')')){
+                    } else {
                         stack.push(c);
-                    }else{
-                        result.append("no\n");
-                        balanced = false;
-                        break;
                     }
                 }
             }
-            if(balanced)
-                result.append("yes\n");
+            result.append(stack.isEmpty() ? "yes\n" : "no\n");
         }
         System.out.println(result);
         in.close();
