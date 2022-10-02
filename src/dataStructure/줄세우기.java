9be77d62af5afe06e9522839ae87bfce8e46c34d
OJH <saintgray0@gmail.com>
2022-10-02 18:04:46 +0900

    BaekJoon : dataStructure 2605 : 줄세우기

---
+++ src/dataStructure/줄세우기.java
@@ -0,0 +1,44 @@
+package dataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+
+//    2605 : 줄세우기
+//    ref url : https://www.acmicpc.net/problem/2605
+
+public class 줄세우기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> result = new Stack<>();
+        Stack<Integer> tempStack = new Stack<>();
+
+        int students = Integer.parseInt(in.readLine());
+        int[] tickets = Arrays.stream(in.readLine().split(" ")).mapToInt(e -> Integer.parseInt(e)).toArray();
+        for (int i = 0; i < students; i++) {
+            reSorting(tempStack, result, tickets[i], i + 1);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Integer num : result)
+            sb.append(String.format("%d ", num));
+        System.out.println(sb);
+        in.close();
+    }
+
+    static void reSorting(Stack<Integer> tempStack, Stack<Integer> result, int ticketNum, int studentNum) {
+        for (int i = 0; i < ticketNum; i++) {
+            if (!result.isEmpty())
+                tempStack.push(result.pop());
+        }
+        result.push(studentNum);
+        align(tempStack, result);
+    }
+
+    static void align(Stack<Integer> tempStack, Stack<Integer> result) {
+        while (!tempStack.isEmpty()) {
+            result.push(tempStack.pop());
+        }
+    }
+}
