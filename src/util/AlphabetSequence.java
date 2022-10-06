1c819da73986bf5a42d8082d6f5d37ab21aabf42
OJH <saintgray0@gmail.com>
2022-10-06 23:33:52 +0900

    util : 알파벳 시퀀스 - 52 진수 시퀀스 발번 테스트 - 최대 2756 개의 시퀀스 발번 가능

--- src/util/AlphabetSequence.java
+++ src/util/AlphabetSequence.java
@@ -6,34 +6,35 @@ import java.util.Stack;
 
 public class AlphabetSequence {
 
-    public class Main {
 
-        public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-            for(int decimal = 27; decimal < 701; decimal++){
-                // 27 : AA(26진수) 의 10진수 변환 , 701 : ZY(26진수) 의 10진수 변환
-                out.write(String.format("%s\n",generateAlphabetSeq(decimal)));
-            }
+        for (int decimal = 53; decimal < 2756; decimal++) {
+            // AA(10 진수 53) ~ zy(10 진수 2755) 시퀀스 발번 테스트
+            out.write(String.format("%s\n", generateConstructJobSequence(decimal)));
+        }
             out.flush();
             out.close();
         }
 
-        static String generateAlphabetSeq(int decimal) throws Exception{
+        static String generateConstructJobSequence(int decimal) throws Exception {
+
             int nextDecimal = decimal + 1;
             Stack<Integer> stack = new Stack<>();
-            while(nextDecimal  > 26){
-                int remain = nextDecimal % 26;
-                stack.push(remain == 0 ? 26 : remain);
-                nextDecimal = remain == 0 ? ((nextDecimal / 26) -1) : nextDecimal / 26;
+            while (nextDecimal > 52) {
+                int remain = nextDecimal % 52;
+                stack.push(remain == 0 ? 52 : remain);
+                nextDecimal = remain == 0 ? ((nextDecimal / 52) - 1) : nextDecimal / 52;
             }
             stack.push(nextDecimal);
-            StringBuilder nextCode = new StringBuilder();
-            while(!stack.isEmpty())
-                nextCode.append((char)(stack.pop() + 64));
 
-            if(nextCode.length() >= 3)
+            if(stack.size() > 2)
                 throw new Exception("자리수 초과");
+            StringBuilder nextCode = new StringBuilder();
+            while (!stack.isEmpty()) {
+                int num = stack.pop() + 64;
+                nextCode.append((char) (num <= 90 ? num : num + 6));
+            }
             return nextCode.toString();
         }
-    }
 }
