6eb84fc6067bd916c6319149666687e22887ec89
saintgray0 <saintgray0@gmail.com>
2024-10-06 13:44:39 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,82 @@
-// 간단한 문제에 대한 제출용 Main Class 입니다
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
-import java.util.stream.Collectors;
-// https://www.acmicpc.net/problem/4386
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Main {
+
     static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws Exception {
-        int N = Integer.parseInt(in.readLine());
-        V[] stars = new V[N];
-        for (int i = 0; i < N; i++) {
-            String[] parms = in.readLine().split(" ");
-            stars[i] = new V(Double.parseDouble(parms[0]), Double.parseDouble(parms[2]));
+    static int N, M, K;
+
+    public static void main(String[] args) throws IOException {
+        String[] params = in.readLine().split(" ");
+        N = Integer.parseInt(params[0]);
+        M = Integer.parseInt(params[1]);
+        K = Integer.parseInt(params[2]);
+        V[] vtx = new V[N + 1];
+        for (int n = 1; n <= N; n++) {
+            vtx[n] = new V(K);
         }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if(i == j) continue;
-                // stars[i].line.add(new E(stars[i], stars[j],))
+        for (int m = 0; m < M; m++) {
+
+            params = in.readLine().split(" ");
+            V from = vtx[Integer.parseInt(params[0])];
+            V to = vtx[Integer.parseInt(params[1])];
+            int d = Integer.parseInt(params[2]);
+            from.roads.add(new E(to, d));
+            to.roads.add(new E(from, d));
+        }
+        Queue<V> queue = new LinkedList<>();
+        vtx[1].min = 0;
+        queue.add(vtx[1]);
+        // 도로를 포장하지 않고 서울에서 포천까지 가는 최단시간 구하기
+        while (!queue.isEmpty()) {
+            V from = queue.poll();
+            if (from.checked) continue;
+            from.checked = true;
+            PriorityQueue<E> roads = from.roads;
+            for (E road : roads) {
+                V to = road.to;
+                if (from.min + road.time < to.min) {
+                    to.checked = false;
+                    to.min = from.min + road.time;
+                    to.prev = from;
+                    queue.add(to);
+                }
             }
         }
+
     }
 
+    /* 정점 정보 */
     static class V {
-        double x;
-        double y;
+        long min;    // 정점 도달까지의 최단 시간
         boolean checked;
-        PriorityQueue<E> line = new PriorityQueue<>();
+        V prev;      // 최단경로로 도달하기 위해 거쳐야 하는 직전 정점
+        PriorityQueue<E> roads;
 
-        public V(double x, double y) {
-            this.x = x;
-            this.y = y;
-            this.line = new PriorityQueue<>();
+        public V(int k) {
+            this.roads = new PriorityQueue<>();
+            this.min = Long.MAX_VALUE;
         }
     }
+
+    /* 정점의 간선 정보 */
     static class E implements Comparable<E> {
-        V u;
-        V v;
-        int d;
+        V to;
+        long time;
+
+        public E(V to, long t) {
+            this.to = to;
+            this.time = t;
+        }
 
         @Override
         public int compareTo(E e) {
-            return Integer.compare(this.d, e.d);
+            return Long.compare(this.time, e.time);
         }
     }
+
 }
