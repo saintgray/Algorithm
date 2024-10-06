8518ec62d40472dca6167d916413f22ee576e01b
OJH <saintgray0@gmail.com>
2024-10-07 00:29:21 +0900

    dataStructure 1167 : 트리의 지름

--- src/dataStructure/트리의지름.java
+++ src/dataStructure/트리의지름.java
@@ -1,4 +1,4 @@
-package graphTheroy;
+package dataStructure;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,39 +7,36 @@ import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-//    1595 : 북쪽나라의 도로
-//    ref url : https://www.acmicpc.net/problem/1595
+//    1167 : 트리의 지름
+//    ref url : https://www.acmicpc.net/problem/1167
 //    tag : tree, diameter of tree
-public class 북쪽나라의도로 {
+public class 트리의지름 {
     static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        V[] vertex = new V[10001];
-        for (int i = 1; i <= 10000; i++) {
+        int N = Integer.parseInt(in.readLine());
+        V[] vertex = new V[N + 1];
+        for (int i = 1; i <= vertex.length - 1; i++) {
             vertex[i] = new V(i);
         }
         String input = null;
         V start = null;
-        while ((input = in.readLine()) != null && input.length() > 0) {
-            String[] param = input.split(" ");
+
+        for (int i = 0; i < N; i++) {
+            String[] param = in.readLine().split(" ");
             V u = vertex[Integer.parseInt(param[0])];
-            V v = vertex[Integer.parseInt(param[1])];
-            int d = Integer.parseInt(param[2]);
-            u.lines.add(new E(v, d));
-            v.lines.add(new E(u, d));
+            for (int j = 1; j < param.length - 1; j+=2) {
+                V v = vertex[Integer.parseInt(param[j])];
+                u.lines.add(new E(v, Integer.parseInt(param[j + 1])));
+                v.lines.add(new E(u, Integer.parseInt(param[j + 1])));
+            }
             if (start == null) start = u;
         }
 
-        // 입력이 없을 경우
-        if (start == null) {
-            System.out.println(0);
-            System.exit(0);
-        }
-
-        // 임의 정점으로부터 가장 거리가 먼 정점(=reef node)을 탐색
-        int radius = 0;
+        // 임의 정점(=start)으로부터 가장 거리가 먼 정점(=reef node)을 탐색
+        long radius = 0;
         V reef = null;
-        start.d = 0;
+        start.d = 0L;
         Queue<V> queue = new LinkedList<>();
         queue.add(start);
         start.visited = true;
@@ -63,7 +60,7 @@ public class 북쪽나라의도로 {
         for (int i = 0; i < vertex.length; i++) {
             if (vertex[i] != null) {
                 vertex[i].visited = false;
-                vertex[i].d = Integer.MIN_VALUE;
+                vertex[i].d = Long.MIN_VALUE;
             }
         }
 
@@ -89,29 +86,29 @@ public class 북쪽나라의도로 {
 
     static class V {
         int no;
-        int d;
+        long d;
         boolean visited;
         PriorityQueue<E> lines;
 
         public V(int no) {
             this.no = no;
             this.lines = new PriorityQueue<>();
-            this.d = Integer.MIN_VALUE;
+            this.d = Long.MIN_VALUE;
         }
     }
 
     static class E implements Comparable<E> {
         V to;
-        int d;  // 거리
+        long d;  // 거리
 
-        public E(V to, int d) {
+        public E(V to, long d) {
             this.to = to;
             this.d = d;
         }
 
         @Override
         public int compareTo(E e) {
-            return -1 * Integer.compare(this.d, e.d);
+            return -1 * Long.compare(this.d, e.d);
         }
     }
 }
